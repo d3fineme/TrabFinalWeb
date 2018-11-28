1fcbafabc146ea2c841951ee9ca5154125f9a5ee
d3fineme <felipe-alves-09@hotmail.com>
2018-11-28 18:12:00 -0200

    email
    função de enviar email de confirmação finalizado

--- views/checkout3.php
+++ views/checkout3.php
@@ -79,18 +79,63 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         <!-- apresentação de qualquer livro, se clicar numa categoria, aparece os daquela categori, portanto é bom colocar um arquivo showBooks.php -->
         <br><br><br><br><br><br><br><br>
         <div class="container #e0e0e0 grey lighten-2">
-            <div class="row center">
-                <div class="col s10 m10 l10 push-l1">
+        <div class="row center">
+        <div class="col s10 m10 l10 push-l1">
+        <?php
+        //To do:
+        // 1. Build sql statement containing ISBNs. Use foreach loop.
+        // 2. Execute sql and display book titles, prices, qty, etc.
+        if (count($bookArray)) {
+            $ctrOrder = new ctrOrder();
+            $pedido = $ctrOrder->pedido($res['custID'], date('y/m/d') );?>
+            <h4><strong>Número do Pedido: </strong><?php echo $pedido; ?></h4>
+            <p><strong>Dados de entrega:</strong> <?php echo $res['fname']; echo ' '; echo $res['lname']; echo "<br>"; $res['street']; echo " - "; echo $res['city'];
+                echo " - "; echo $res['state']; echo " - "; echo $res['zip']?></p>
+            <br>
+            <table id='cart'>
+                <tr>
+                    <th>Título</th>
+                    <th> Quantidade </th>
+                    <th> Preço Unitário</th>
+                    <th> Preço Total</th>
+                </tr>
+                <?php
+                foreach ($bookArray as $isbn => $qty) {
+
+                    ?>
+                    <tr>
+                        <td>
+                            <a class='booktitle' href='ProductPage.php?isbn=<?=$isbn?>'><?php $book = $ctrBook->retornaLivro($isbn);
+                                echo $book['title'];?></a> </td>
+                        <td><?=$qty?></td>
+                        <td>
+                            <?php
+                            $pc = $ctrBook->retornaLivro($isbn);
+                            echo $pc['price'];
+                            ?>
+                        </td>
+                        <td>
+                            <?php echo $pc['price']*$qty?>
+                        </td>
+                    </tr>
+
                     <?php
-                    //To do:
-                    // 1. Build sql statement containing ISBNs. Use foreach loop.
-                    // 2. Execute sql and display book titles, prices, qty, etc.
-                    if (count($bookArray)) {
-                        $ctrOrder = new ctrOrder();
-                        $pedido = $ctrOrder->pedido($res['custID'], date('y/m/d') );?>
-                        <h4><strong>Número do Pedido: </strong><?php echo $pedido; ?></h4>
-                        <p><strong>Dados de entrega:</strong> <?php echo $res['fname']; echo ' '; echo $res['lname']; echo "<br>"; $res['street']; echo " - "; echo $res['city'];
-                                                                echo " - "; echo $res['state']; echo " - "; echo $res['zip']?></p>
+                    $ctrOrder = new ctrOrder();
+                    $ctrOrder->itensPedido($pedido,$isbn,$qty, $pc['price']);
+                }
+                ?>
+            </table>
+            <p><strong>Sub-total: </strong>R$<?php echo number_format($valor,2); ?></p>
+            <p><strong>Frete: </strong>R$<?php echo $frete; ?></p>
+            <p><strong>Valor final: </strong><?php echo number_format($valorfinal,2); ?></p>
+            </div>
+            </div>
+            <?php
+            $headers = "From: Felipe Alves <felipe.unifei.sin@gmail.com> \r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=UTF-8 \r\n";
+            $emaildecompra1 = "<html><body><h4><strong>Número do Pedido: </strong>".$pedido."</h4>
+                        <p><strong>Dados de entrega:</strong> ".$res['fname'].' '.$res['lname']."<br>" .$res['street']." - ".$res['city']." - ".$res['state']." - ".$res['zip']."</p>
                         <br>
                         <table id='cart'>
                             <tr>
@@ -98,66 +143,53 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
                                 <th> Quantidade </th>
                                 <th> Preço Unitário</th>
                                 <th> Preço Total</th>
-                            </tr>
-                        <?php
-                        foreach ($bookArray as $isbn => $qty) {
-
-                            ?>
-                            <tr>
-                                <td>
-                                    <a class='booktitle' href='ProductPage.php?isbn=<?=$isbn?>'><?php $book = $ctrBook->retornaLivro($isbn);
-                                                                                                    echo $book['title'];?></a> </td>
-                                <td><?=$qty?></td>
-                                <td>
-                                    <?php
-                                    $pc = $ctrBook->retornaLivro($isbn);
-                                    echo $pc['price'];
-                                    ?>
+                            </tr>";
+            foreach ($bookArray as $isbn => $qty) {
+                $book = $ctrBook->retornaLivro($isbn);
+                $pc = $ctrBook->retornaLivro($isbn);
+                $emaildecompra2 = "<tr>
+                                <td>".$book['title']." </td>
+                                <td>".$qty."</td>
+                                <td>".$pc['price']."
                                 </td>
-                                <td>
-                                    <?php echo $pc['price']*$qty?>
+                                <td>".$pc['price']*$qty."
                                 </td>
-                            </tr>
+                            </tr>";
+            }
+            $emaildecompra3 = "</table>
+            <p><strong>Sub-total: </strong>R$".number_format($valor,2)."</p>
+            <p><strong>Frete: </strong>R$".$frete."</p>
+            <p><strong>Valor final: </strong>".number_format($valorfinal,2)."</p></body></html>";
+            ?>
+            <?php
+            mail($res['email'], 'Comprovante de compra realizada na TechBook' ,$emaildecompra1.$emaildecompra2.$emaildecompra3, $headers);
+        } else{
+            ?>
+            <br>
+            <div class="container #e0e0e0 grey lighten-2">
+                <div class="row center">
+                    <div class="col s12 m12 l12">
+                        <h3><p>Seu carrinho está vazio, <a href="index.php">Vamos comprar?</a></p></h3>
+                    </div>
+                </div>
+                <div class="row">
 
-                        <?php
-                        $ctrOrder = new ctrOrder();
-                        $ctrOrder->itensPedido($pedido,$isbn,$qty, $pc['price']);
-                        }
-                        ?>
-                        </table>
-                        <p><strong>Sub-total: </strong>R$<?php echo number_format($valor,2); ?></p>
-                        <p><strong>Frete: </strong>R$<?php echo $frete; ?></p>
-                        <p><strong>Valor final: </strong><?php echo number_format($valorfinal,2); ?></p>
-                        </div>
-                        </div>
-                        <?php
-                    } else{
-                        ?>
-                        <br>
-                        <div class="container #e0e0e0 grey lighten-2">
-                            <div class="row center">
-                                <div class="col s12 m12 l12">
-                                    <h3><p>Seu carrinho está vazio, <a href="index.php">Vamos comprar?</a></p></h3>
-                                </div>
-                            </div>
-                            <div class="row">
-
-                            </div>
-                        </div>
-                        </div>
-                        <?php
-                    }
-
-                    ?>
                 </div>
             </div>
-            <div class="row">
-                <div class="col s4 m4 l4 push-l2">
-                    <a href="orderHistory.php?id=<?php echo $res['custID'] ?>" class="btn waves-effect waves-light">Ver histórico de pedido
-                        <i class="material-icons right">access_time</i>
-                    </a>
-                </div>
             </div>
+            <?php
+        }
+
+        ?>
+        </div>
+        </div>
+        <div class="row">
+            <div class="col s4 m4 l4 push-l2">
+                <a href="orderHistory.php?id=<?php echo $res['custID'] ?>" class="btn waves-effect waves-light">Ver histórico de pedido
+                    <i class="material-icons right">access_time</i>
+                </a>
+            </div>
+        </div>
         </div>
         <br><br><br><br><br><br><br><br>
 
@@ -174,66 +206,99 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         $res = $ctrUser->cadastrar($email, $name, $sobrenome, $street, $city, $state, $zip);
         if ($res) {
             ?>
+            <br><br>
             <div class="container #e0e0e0 grey lighten-2">
-                <div class="row center">
-                    <div class="col s10 m10 l10 push-l1">
-            <?php
-            if (count($bookArray)) {
-                $ctrOrder = new ctrOrder();
-                $pedido = $ctrOrder->pedido($res['custID'], date('y/m/d')); ?>
-                <h4><strong>Número do Pedido: </strong><?php echo $pedido; ?></h4>
-                <p><strong>Dados de entrega:</strong> <?php echo $res['fname'];
-                    echo ' ';
-                    echo $res['lname'];
-                    echo "<br>";
-                    $res['street'];
-                    echo " - ";
-                    echo $res['city'];
-                    echo " - ";
-                    echo $res['state'];
-                    echo " - ";
-                    echo $res['zip'] ?></p>
-                <br>
-                <table id='cart'>
-                    <tr>
-                        <th>Título</th>
-                        <th> Quantidade</th>
-                        <th> Preço Unitário</th>
-                        <th> Preço Total</th>
-                    </tr>
+            <div class="row center">
+                <div class="col s10 m10 l10 push-l1">
                     <?php
-                    foreach ($bookArray as $isbn => $qty) {
-
-                        ?>
+                    if (count($bookArray)) {
+                    $ctrOrder = new ctrOrder();
+                    $pedido = $ctrOrder->pedido($res['custID'], date('y/m/d')); ?>
+                    <h4><strong>Número do Pedido: </strong><?php echo $pedido; ?></h4>
+                    <p><strong>Dados de entrega:</strong> <?php echo $res['fname'];
+                        echo ' ';
+                        echo $res['lname'];
+                        echo "<br>";
+                        $res['street'];
+                        echo " - ";
+                        echo $res['city'];
+                        echo " - ";
+                        echo $res['state'];
+                        echo " - ";
+                        echo $res['zip'] ?></p>
+                    <br>
+                    <table id='cart'>
                         <tr>
-                            <td>
-                                <a class='booktitle'
-                                   href='ProductPage.php?isbn=<?= $isbn ?>'><?php $book = $ctrBook->retornaLivro($isbn);
-                                    echo $book['title']; ?></a></td>
-                            <td><?= $qty ?></td>
-                            <td>
-                                <?php
-                                $pc = $ctrBook->retornaLivro($isbn);
-                                echo $pc['price'];
-                                ?>
-                            </td>
-                            <td>
-                                <?php echo $pc['price'] * $qty ?>
-                            </td>
+                            <th>Título</th>
+                            <th> Quantidade</th>
+                            <th> Preço Unitário</th>
+                            <th> Preço Total</th>
                         </tr>
-
                         <?php
-                        $ctrOrder = new ctrOrder();
-                        $ctrOrder->itensPedido($pedido, $isbn, $qty, $pc['price']);
-                    }
-                    ?>
-                </table>
-                <p><strong>Sub-total: </strong>R$<?php echo number_format($valor, 2); ?></p>
-                <p><strong>Frete: </strong>R$<?php echo $frete; ?></p>
-                <p><strong>Valor final: </strong><?php echo number_format($valorfinal, 2); ?></p>
+                        foreach ($bookArray as $isbn => $qty) {
+
+                            ?>
+                            <tr>
+                                <td>
+                                    <a class='booktitle'
+                                       href='ProductPage.php?isbn=<?= $isbn ?>'><?php $book = $ctrBook->retornaLivro($isbn);
+                                        echo $book['title']; ?></a></td>
+                                <td><?= $qty ?></td>
+                                <td>
+                                    <?php
+                                    $pc = $ctrBook->retornaLivro($isbn);
+                                    echo $pc['price'];
+                                    ?>
+                                </td>
+                                <td>
+                                    <?php echo $pc['price'] * $qty ?>
+                                </td>
+                            </tr>
+
+                            <?php
+                            $ctrOrder = new ctrOrder();
+                            $ctrOrder->itensPedido($pedido, $isbn, $qty, $pc['price']);
+                        }
+                        ?>
+                    </table>
+                    <p><strong>Sub-total: </strong>R$<?php echo number_format($valor, 2); ?></p>
+                    <p><strong>Frete: </strong>R$<?php echo $frete; ?></p>
+                    <p><strong>Valor final: </strong><?php echo number_format($valorfinal, 2); ?></p>
                 </div>
-                </div>
-                <?php
+            </div>
+            <?php
+            $headers = "From: Felipe Alves <felipe.unifei.sin@gmail.com> \r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: text/html; charset=UTF-8     \r\n";
+            $emaildecompra1 = "<html><body><h4><strong>Número do Pedido: </strong>".$pedido."</h4>
+                        <p><strong>Dados de entrega:</strong> ".$res['fname'].' '.$res['lname']."<br>" .$res['street']." - ".$res['city']." - ".$res['state']." - ".$res['zip']."</p>
+                        <br>
+                        <table id='cart'>
+                            <tr>
+                                <th>Título</th>
+                                <th> Quantidade </th>
+                                <th> Preço Unitário</th>
+                                <th> Preço Total</th>
+                            </tr>";
+            foreach ($bookArray as $isbn => $qty) {
+                $book = $ctrBook->retornaLivro($isbn);
+                $pc = $ctrBook->retornaLivro($isbn);
+                $emaildecompra2 = "<tr>
+                                <td>".$book['title']." </td>
+                                <td>".$qty."</td>
+                                <td>".$pc['price']."
+                                </td>
+                                <td>".$pc['price']*$qty."
+                                </td>
+                            </tr>";
+            }
+            $emaildecompra3 = "</table>
+            <p><strong>Sub-total: </strong>R$".number_format($valor,2)."</p>
+            <p><strong>Frete: </strong>R$".$frete."</p>
+            <p><strong>Valor final: </strong>".number_format($valorfinal,2)."</p></body></html>";
+            ?>
+            <?php
+                mail($res['email'], 'Comprovante de compra realizada na TechBook' ,$emaildecompra1.$emaildecompra2.$emaildecompra3, $headers);
             } else {
                 ?>
                 <br>
@@ -257,7 +322,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
                     </a>
                 </div>
             </div>
-                <?php
+            <?php
         }
     }
 }
